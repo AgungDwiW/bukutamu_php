01e1d56ef8338ea2cc38c9b0dd4a466241b329ab
erdk <magzksky@gmail.com>
2019-06-24 10:10:30 +0700

    Fixed FE

---
+++ pelaporan/navbar.php
@@ -0,0 +1,93 @@
+ <nav class="main-header navbar-default navbar navbar-inverse navbar-expand bg-white navbar-light border-bottom fixed-top" >
+      <div class="container-fluid">
+              <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+           
+             </li>
+           
+        
+        </ul>
+
+        <!-- SEARCH FORM -->
+
+        <!-- Right navbar links -->
+        <ul class="nav navbar-nav ml-auto">
+
+            <li class="nav-item">
+                <a href="" class="nav-link" style="color: DodgerBlue;" data-toggle="modal" data-target="#exampleModal"><i
+                        class="fa fa-fw fa-sign-out"></i>
+                    <span class="nav-link-text">Keluar</span></a>
+            </li>
+        </ul>
+    </div>
+         
+
+    </nav>
+   
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    
+    <aside class="main-sidebar sidebar-dark-primary elevation-4" style="min-height: 450px !important;">
+        <a href="{%url 'pelaporan:bukutamu'" class="brand-link">   
+            <img src="../assets/img/logo.png"  alt="Logo Aqua" class="brand-image img-circle elevation-3"
+                style="opacity: .8">
+            <span class="brand-text font-weight-light">Pelaporan</span>
+        </a>
+        <div class="sidebar">
+            <!-- Sidebar user panel (optional) -->
+
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                    data-accordion="false">
+                    <!-- Add icons to the links using the .nav-icon class
+                               with font-awesome or any other icon font library -->
+                    <li class="nav-item">
+                        <a href="dashboard.php" class="nav-link">
+                            <i class="nav-icon fa  fa-area-chart" aria-hidden="true"></i>
+                            <p>
+                                Dashboard
+                            </p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="pelaporan.php" class="nav-link">
+                            <i class="nav-icon fa fa-file" aria-hidden="true"></i>
+                            <p>
+                                Form Pelaporan
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="listpelaporan.php" class="nav-link">
+                            <i class="nav-icon fa fa-file" aria-hidden="true"></i>
+                            <p>
+                                List Pelaporan
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item" >
+                        <a href="bukutamu.php" class="nav-link">
+                            <i class="nav-icon fa fa-book" aria-hidden="true"></i>
+                            <p>
+                                Buku tamu
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item" >
+                        <a href="users.php" class="nav-link">
+                            <i class="nav-icon fa fa-user"></i>
+                            <p>
+                                List tamu
+                            </p>
+                        </a>
+                        </li>
+                </ul>
+            </nav>
+            <!-- /.sidebar-menu -->
+        </div>
+        <!-- /.sidebar -->
+    </aside>
