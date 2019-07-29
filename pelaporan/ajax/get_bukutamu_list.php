3e052c04c5e4bbf78b241dc8d129f0c98899f7c8
AgungDwiW <agung.dwi.temp@gmail.com>
2019-07-29 10:24:07 +0700

    bukutanu filter shift

--- pelaporan/ajax/get_bukutamu_list.php
+++ pelaporan/ajax/get_bukutamu_list.php
@@ -16,17 +16,34 @@
 			require "../../db/db_con.php";
 			$start = $_POST['start'];
 			$end = $_POST['end'];
-			// echo json_encode($_POST);
-			// $return ['1'] = "SELECT * from kedatangan where";
-			// $return ['2'] =  "STR_TO_DATE(tanggal_datang) > STR_TO_DATE('".$start.") and STR_TO_DATE(tanggal_datang) > STR_TO_DATE(".$end.") ";
-			// $return['3'] = "SELECT * from kedatangan where ".
-			//   "STR_TO_DATE(tanggal_datang, '%Y-%m-%d') > STR_TO_DATE('".$start."', '%Y-%m-%d') and STR_TO_DATE(tanggal_datang, '%Y-%m-%d') < STR_TO_DATE('".$end."', '%Y-%m-%d') ";
+			$flag = $_POST['shift'];
+
+			if ($flag == 1){
+				$start_hour = "06:00:01";
+				$end_hour = "14:00:00";
+			}
+			else if ($flag ==2){
+				$start_hour = "14:00:01";
+				$end_hour = "22:00:00";		
+			}
+			else if ($flag ==3){
+				$start_hour = "22:00:01";
+				$end_hour = "06:00:00";		
+			}
+			else if ($flag ==4){
+				$start_hour = "00:00:00";
+				$end_hour = "23:59:59";		
+			}
+			else
+			{	
+				$return['error'] = "parameter not satisfied";
+				echo json_encode($return);
+				return false;
+			}
+
 			$sql = "SELECT * from kedatangan where ".
-			  "STR_TO_DATE(tanggal_datang, '%Y-%m-%d') >= STR_TO_DATE('".$start."', '%Y-%m-%d') and STR_TO_DATE(tanggal_datang, '%Y-%m-%d') <= STR_TO_DATE('".$end."', '%Y-%m-%d') ";
-			// $return ['sql'] = $sql;
-			// $return['bobo'] ="qontol";
-			// echo json_encode($return);			
-			// return true; 
+			  "STR_TO_DATE(tanggal_datang, '%Y-%m-%d') >= STR_TO_DATE('".$start."', '%Y-%m-%d') and STR_TO_DATE(tanggal_datang, '%Y-%m-%d') <= STR_TO_DATE('".$end."', '%Y-%m-%d') and DATE_FORMAT(tanggal_datang,'%H:%i:%s')>= STR_TO_DATE('".$start_hour."', '%H:%i:%s') and  DATE_FORMAT(tanggal_datang,'%H:%i:%s') <= STR_TO_DATE('".$end_hour."','%H:%i:%s') ";
+			
 			$result = mysqli_query($conn, $sql);
 			if ($result && mysqli_num_rows($result) !=0){
 			$return=array();
